853d09029ef5191dc2649f8209179771f9e055f1
Jack Pope <jack@byramdigitalmedia.com>
2023-12-05 15:25:00 -0500

    first commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Intro() {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Jack Pope</h1>
+      <p className="text-base md:text-xl mb-3 font-medium">Front-End Web Developer</p>
+      <p className="text-sm max-w-xl mb-6 font-bold">Thanks for visiting! I have been studying web development since July of 2018. During this time I have gained solid intermediate skills in HTML, CSS, and JS, and earned the Web Development Certificate from County College of Morris in Randolph, NJ, in 2022. This program has also provided me experience with WordPress, Dreamweaver, Photoshop, Java, and MySQL.</p>
+      <p className="text-base md:text-xl mb-3 font-medium">Eager to Work!</p>
+      <p className="text-base md:text-xl mb-3 font-medium">(973) 349-6522</p>
+      <p className="text-base md:text-xl mb-3 font-medium">jack@ByramDigitalMedia.com</p>
+      <p>Byram, NJ</p>
+    </div>
+  )
+}
+
+export default Intro
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "True Journey Books",
+    imgUrl: "/assets/tj.jpg",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://jwilliampope.github.io/true-journey/"
+  },
+  {
+    title: "Bonner Family Archive",
+    imgUrl: "/assets/bonner.jpg",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://jwilliampope.github.io/BonnerFamilyArchive/"
+  },
+  {
+    title: "Byram Digital Media",
+    imgUrl: "/assets/bdm.jpg",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "https://byramdigitalmedia.com/"
+  }
+]
